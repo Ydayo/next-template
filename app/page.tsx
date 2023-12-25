047b85d85b1c71748086c4f32633af9497a67400
IshikawaTokyo <yyusb.mac@gmail.com>
2023-12-25 10:07:28 +0900

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/atoms/Button/button";
+
+export default function Home() {
+  return (
+    <>
+      <p className="text-[25px] font-bold text-red-500">Next Template</p>
+      <Button className="bg-red-900">Click</Button>
+    </>
+  );
+}
